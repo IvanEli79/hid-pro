f944084398b711625228edf6fb50918ebb214271
One Evil Programmer <oneEvilProgrammer@gmail.com>
2023-11-23 20:35:25 -0600

    Settings Commit to GitHub 20231123_2035

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://ivaneli79.github.io',
+  base: '/hid-pro',
 });
